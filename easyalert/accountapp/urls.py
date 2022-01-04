d42cf6d907ded556dd2ee2bd9fc9655aa7455519
Adrian123K <38353340+Adrian123K@users.noreply.github.com>
2022-01-04 17:49:34 +0900

    220104 com1

---
+++ easyalert/accountapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from accountapp.views import hello_world
+
+# appName = 'accountapp'
+
+
+urlpatterns = [
+    path('hello_world', hello_world, name='hello_world')
+]
