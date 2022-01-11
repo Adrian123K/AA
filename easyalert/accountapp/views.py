21208d17ad15d3cd0ef9b25f9ea5005d9d029d47
Adrian <seojun.a.kim@gmail.com>
2022-01-11 17:24:39 +0900

    220111 html, view파일 수정2

--- easyalert/accountapp/views.py
+++ easyalert/accountapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from accountapp.models import HelloWorld
+
 # Create your views here.
 
 def hello_world(request):
@@ -9,7 +11,12 @@ def hello_world(request):
         
         temp = request.POST.get('hello_world_input')
         
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save() # DB에 저장
+
         
-        return render(request, 'accountapp/hello_world.html', context={"text":temp})
+        
+        return render(request, 'accountapp/hello_world.html', context={"hello_world_output":new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={"text":"GET METHOD"})
