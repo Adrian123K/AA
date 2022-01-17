96c79d140ecd77076782f41db5f1f5ffcb5a23ce
Adrian123K <38353340+Adrian123K@users.noreply.github.com>
2022-01-17 18:36:36 +0900

    220117 view수정(update, delete)

--- easyalert/articleapp/models.py
+++ easyalert/articleapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+    
+    # 게시글 작성 날짜를 자동으로 등록
+    created_at = models.DateField(auto_now_add=True, null=True)
